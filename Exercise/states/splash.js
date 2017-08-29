32285e2b0932075e2c1f6d7aaacc5e3a4016731a
KYUNGMIN <lufovic77@naver.com>
2017-08-29 17:52:41 +0900

    17/08/29

--- Exercise/states/splash.js
+++ Exercise/states/splash.js
@@ -1,34 +1,52 @@
 Splash.prototype = {
 
-  loadScripts: function () {
+   loadScripts: function () {
+    // You can download this at: https://github.com/typekit/webfontloader
+    // or use npm install webfontloader
+    game.load.script('WebFont', 'vendor/webfontloader.js');
+    // You can go ahead and make empty versions of these:
+    game.load.script('gamemenu','states/gamemenu.js');
+    game.load.script('thegame', 'states/thegame.js');
+    game.load.script('gameover','states/gameover.js');
+    game.load.script('credits', 'states/credits.js');
+    game.load.script('options', 'states/options.js');
   },
 
   loadBgm: function () {
+    // thanks Kevin Macleod at http://incompetech.com/
+    game.load.audio('dangerous', 'assets/bgm/Dangerous.mp3');
+    game.load.audio('exit', 'assets/bgm/Exit the Premises.mp3');
   },
-
+  // various freebies found from google image search
   loadImages: function () {
+    game.load.image('menu-bg', 'assets/images/menu-bg.jpg');
+    game.load.image('options-bg', 'assets/images/options-bg.jpg');
+    game.load.image('gameover-bg', 'assets/images/gameover-bg.jpg');
   },
 
   loadFonts: function () {
   },
 
+
+ init: function () {
+    this.loadingBar = game.make.sprite(game.world.centerX-(387/2), 400, "loading");
+    this.logo       = game.make.sprite(game.world.centerX, 200, 'brand');
+    this.status     = game.make.text(game.world.centerX, 380, 'Loading...', {fill: 'white'});
+    utils.centerGameObjects([this.logo, this.status]);
+  },
   // The preload function then will call all of the previously defined functions:
   preload: function () {
-    var myLogo, loadingBar, status;
+
     game.add.sprite(0, 0, 'stars');
+    game.add.existing(this.logo).scale.setTo(0.5);
+    game.add.existing(this.loadingBar);
+    game.add.existing(this.status);
+    this.load.setPreloadSprite(this.loadingBar);
 
-    myLogo = game.add.sprite(game.world.centerX, 100, 'brand');
-    myLogo.anchor.setTo(0.5);
-    myLogo.scale.setTo(0.5);
-
-    status = game.add.text(game.world.centerX, 380, 'Loading...', {fill: 'white'});
-    status.anchor.setTo(0.5);
-
-    loadingBar = game.add.sprite(game.world.centerX, 400, "loading");
-    loadingBar.anchor.setTo(0.5);
-    this.load.setPreloadSprite(loadingBar);
+    
     this.loadScripts();
     this.loadImages();
     this.loadFonts();
     this.loadBgm();
-  },
+  }
+}
