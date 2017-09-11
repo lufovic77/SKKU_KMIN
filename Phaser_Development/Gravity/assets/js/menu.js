f99c0a577264fceb2c32e20f7478ff3fb0a54b2e
KYUNGMIN <lufovic77@naver.com>
2017-09-11 14:34:06 +0900

    folder renamed

--- Phaser_Development/Gravity/assets/js/menu.js
+++ Phaser_Development/Gravity/assets/js/menu.js
@@ -2,16 +2,33 @@ var start;
 var music;
 var titleText;
 var startText,optionText;
+var text = null;
+var grd;
+
+WebFontConfig = {
+
+    //  'active' means all requested fonts have finished loading
+    //  We set a 1 second delay before calling 'createText'.
+    //  For some reason if we don't the browser cannot render the text the first time it's created.
+    active: function() { game.time.events.add(Phaser.Timer.SECOND, this.createText, this); },
+
+    //  The Google Fonts we want to load (specify as many as you like in the array)
+    google: {
+      families: ['Revalia']
+    }
+
+};
 var Menu = {
     preload : function() {
         // load menu image form game object
+   // game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
         game.load.image('back', './assets/images/back.png');
         game.load.image('start', './assets/images/startButton.png');
 
     game.load.audio('background','./assets/musics/Marimba-Boy.wav');
     },
 
-    create: function () {
+  init: function () {
     this.titleText = game.make.text(game.world.centerX, 100, "Gravity", {
         font: 'bold 65px Arial',
       fill: '#FDFFB5',
@@ -19,6 +36,9 @@ var Menu = {
     });
     this.titleText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
     this.titleText.anchor.set(0.5);
+   // this.optionCount = 1;
+  },
+    create: function () {
         // add image
     	music = game.sound.play('background');
         game.add.sprite(0, 0, 'back');
@@ -69,6 +89,27 @@ var Menu = {
     optionText.events.onInputUp.add(this.up2, this);
 
     },
+ createText: function() {
+
+    text = game.add.text(game.world.centerX, game.world.centerY, "- phaser -\nrocking with\ngoogle web fonts");
+    text.anchor.setTo(0.5);
+
+    text.font = 'Revalia';
+    text.fontSize = 60;
+
+    //  x0, y0 - x1, y1
+    grd = text.context.createLinearGradient(0, 0, 0, text.canvas.height);
+    grd.addColorStop(0, '#8ED6FF');   
+    grd.addColorStop(1, '#004CB3');
+    text.fill = grd;
+
+    text.align = 'center';
+    text.stroke = '#000000';
+    text.strokeThickness = 2;
+    text.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
+
+
+},
     startOption: function(){
 
     },
